87bf5cfabe01b791222bf20ffebf9d3f3046c6b7
Bamidele Ademokoya <bamyonline@gmail.com>
2023-09-12 23:41:32 +0100

    Initial commit

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'The Alchemist',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'The Secret',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Tuesdays with Morrie',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+    {
+        productName: 'Eat, Pray, Love',
+        productNumber: '12345',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+    {
+        productName: 'The Power of Now',
+        productNumber: '67890',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: "Man's Search for Meaning",
+        productNumber: '13579',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+    {
+        productName: 'The Road Less Traveled',
+        productNumber: '24680',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    }
+];
